5c6d1d73302f8df5ed029e8c618689b1cf009d0b
aniruddha123reinforcement <81187574+aniruddha123reinforcement@users.noreply.github.com>
2022-10-13 21:51:53 -0700

    Update networks.py

--- learn2learn/algorithms/epg/networks.py
+++ learn2learn/algorithms/epg/networks.py
@@ -1,36 +1,16 @@
+import torch 
+import torch.nn.functional as F
+import traceback
+from torch.autograd import grad
+
+from torch import optim
+
+
+
 import chainer as C
 import chainer.functions as F
 import numpy as np
 
-
-class NN(object):
-    """Simple MLP
-    """
-
-    def __init__(self, dims=None, out_fn=None):
-        assert dims is not None
-        assert out_fn is not None
-        assert len(dims) >= 2
-
-        self._out_fn = out_fn
-        self._lst_w, self._lst_b = [], []
-        for i in range(len(dims) - 1):
-            shp = dims[i + 1], dims[i]
-            # Correctly init weights.
-            std = 0.01 if i == len(dims) - 2 else 1.0
-            out = np.random.randn(*shp).astype(np.float32)
-            out *= std / np.sqrt(np.square(out).sum(axis=0, keepdims=True))
-            self._lst_w.append(C.Variable(out))
-            self._lst_b.append(C.Variable(np.zeros(shp[0], dtype=np.float32)))
-        self.train_vars = self._lst_w + self._lst_b
-
-    def f(self, x):
-        for i, (w, b) in enumerate(zip(self._lst_w, self._lst_b)):
-            x = F.linear(x, w, b)
-            if i != len(self._lst_w) - 1:
-                x = F.tanh(x)
-            else:
-                return self._out_fn(x)
 
     def set_params_1d(self, params):
         """Set params for ES (theta)
