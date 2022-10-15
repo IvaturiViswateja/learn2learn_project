db7c25e15ed10a8e43322ae17701065abf1c500a
aniruddha123reinforcement <81187574+aniruddha123reinforcement@users.noreply.github.com>
2022-10-14 18:01:02 -0700

    Update networks.py

--- learn2learn/algorithms/epg/networks.py
+++ learn2learn/algorithms/epg/networks.py
@@ -9,27 +9,7 @@ from torch import optim
 
 
 
-#convert this to pytorch linear model
-class Memory(object):
-    """Linear memory, allows storing information in its weights.
-    """
-
-    def __init__(self, size_in=None, size_out=None):
-        assert size_in is not None
-        assert size_out is not None
-
-        shp = size_out, size_in
-        # Correctly init weights.
-        out = np.random.randn(*shp).astype(np.float32)
-        out *= 0.01 / np.sqrt(np.square(out).sum(axis=0, keepdims=True))
-        self._w = C.Variable(out)
-        self._b = C.Variable(np.zeros(shp[0], dtype=np.float32))
-        self.train_vars = [self._w, self._b]
-
-    def f(self):
-        x = np.ones(self._w.shape[1], dtype=np.float32)[np.newaxis, :]
-        # Needs nonlinearity after memory
-        return F.tanh(F.linear(x, self._w, self._b))
+###pytorch version 
 class Memory(object):
     """Linear memory, allows storing information in its weights.
     """
