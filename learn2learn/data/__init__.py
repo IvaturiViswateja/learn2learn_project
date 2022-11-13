e4df8ffeefba79339d362f4e43ca3ce4ecdaac14
Vis Ivaturi <ivaturi96@gmail.com>
2022-11-13 14:09:09 -0700

    pyximport in __init__

--- learn2learn/data/__init__.py
+++ learn2learn/data/__init__.py
@@ -3,9 +3,10 @@
 r"""
 A set of utilities for data & tasks loading, preprocessing, and sampling.
 """
-
+import pyximport
+pyximport.install()
 # from . import transforms
 from torchvision import transforms
 # from .meta_dataset import MetaDataset, UnionMetaDataset, FilteredMetaDataset
-# from .task_dataset import TaskDataset, DataDescription
+from .task_dataset import TaskDataset, DataDescription
 from .utils import OnDeviceDataset, partition_task, InfiniteIterator
